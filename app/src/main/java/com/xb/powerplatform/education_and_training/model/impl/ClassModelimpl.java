386027eb18b416249b467547ad8ee2fa557bc8d1
dell <914036951>
2018-01-06 10:22:16 +0800

    withoutwork01

--- app/src/main/java/com/xb/powerplatform/education_and_training/model/impl/ClassModelimpl.java
+++ app/src/main/java/com/xb/powerplatform/education_and_training/model/impl/ClassModelimpl.java
@@ -2,13 +2,16 @@ package com.xb.powerplatform.education_and_training.model.impl;
 
 import android.content.Context;
 
+import com.xb.powerplatform.R;
 import com.xb.powerplatform.education_and_training.bean.assess;
 import com.xb.powerplatform.education_and_training.model.ClassModel;
 import com.xb.powerplatform.utilsclass.base.BaseModeBackLisenter;
 import com.xb.powerplatform.utilsclass.base.BaseRequestBackLisenter;
 import com.xb.powerplatform.utilsclass.base.Constant;
+import com.xb.powerplatform.utilsclass.base.MyApplication;
 import com.xb.powerplatform.utilsclass.base.MySubscriberbean;
 import com.xb.powerplatform.utilsclass.base.RetrofitService;
+import com.xb.powerplatform.utilsclass.utils.AlertDialogUtil;
 import com.xb.powerplatform.utilsclass.utils.HttpUtils;
 
 /**
@@ -19,17 +22,23 @@ import com.xb.powerplatform.utilsclass.utils.HttpUtils;
 public class ClassModelimpl implements ClassModel {
 
     @Override
-    public void getModelData(String httpTag, Context context, String id, final BaseModeBackLisenter baseModeBackLisenter) {
+    public void getModelData(String httpTag, final Context context, String id, final BaseModeBackLisenter baseModeBackLisenter) {
         HttpUtils.initlist(HttpUtils.getService(RetrofitService.class).getClass(id)
                 ,new MySubscriberbean(httpTag,context, Constant.DOWNLOAD, new BaseRequestBackLisenter<assess>() {
                     @Override
                     public void success(assess assesses) {
-                        baseModeBackLisenter.success(assesses);
+                        if (assesses.getBody().getBmList().size()==0){
+                            new AlertDialogUtil(context).
+                                    showSmallDialog(MyApplication.getContextObject().getResources().
+                                            getString(R.string.no_assess));
+                        }else {
+                            baseModeBackLisenter.success(assesses);
+                        }
                     }
 
                     @Override
                     public void fail(String message) {
-                        baseModeBackLisenter.error(message);
+                        //baseModeBackLisenter.error(message);
                     }
                 }));
     }
